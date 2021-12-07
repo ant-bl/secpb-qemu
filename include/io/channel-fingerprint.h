322b0d2da81bab3d8874d95fa755ddf3d88b368a
antoine <antoine.blin@gandi.net>
2021-12-07 14:44:52 +0100

    add channel-fingerprint hash

--- include/io/channel-fingerprint.h
+++ include/io/channel-fingerprint.h
@@ -11,6 +11,8 @@
 #define TYPE_QIO_CHANNEL_FINGERPRINT "qio-channel-fingerprint"
 OBJECT_DECLARE_SIMPLE_TYPE(QIOChannelFingerprint, QIO_CHANNEL_FINGERPRINT)
 
+#define CHANNEL_FINGERPRINT_HASH_TYPE "sha1"
+
 QIOChannelFingerprint *
 qio_channel_fingerprint_new(QIOChannel *ioc, char const *fingerprint_path,
                             char const *ram_path, char const *disk_path,
